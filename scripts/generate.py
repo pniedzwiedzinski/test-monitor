4c8eaa24bb659edfc9d81d03b5da8c1635c97cde
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-12-21 23:06:10 +0100

    Add autoreload

--- scripts/generate.py
+++ scripts/generate.py
@@ -3,6 +3,8 @@ import subprocess
 from jinja2 import Template
 
 os.mkdir("dist")
+# Hash of HEAD commit
+head = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1]
 
 ## Generate site
 with open("current_page") as fp:
@@ -12,10 +14,9 @@ with open("scripts/template.html") as fp:
 	template = Template(fp.read())
 
 with open("dist/index.html", "w") as fp:
-	fp.write(template.render(page=page))
+	fp.write(template.render(page=page, head=head))
+
 
 ## Generate `head` file
-
-head = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 with open("dist/head", "w") as fp:
 	fp.write(head)
