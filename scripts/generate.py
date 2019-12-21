0b9b7964552b20a56c3882ae5be2e6365f948da4
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-12-21 22:58:07 +0100

    Add head file

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,8 +1,10 @@
 import os
+import subprocess
 from jinja2 import Template
 
 os.mkdir("dist")
 
+## Generate site
 with open("current_page") as fp:
 	page = fp.read()
 
@@ -12,3 +14,8 @@ with open("scripts/template.html") as fp:
 with open("dist/index.html", "w") as fp:
 	fp.write(template.render(page=page))
 
+## Generate `head` file
+
+head = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+with open("dist/head", "w") as fp:
+	fp.write(head)
